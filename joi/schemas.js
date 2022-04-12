0d37f5ccdb3d0d47f32a92cc01d60867f2d4295d
Jorge Paul <jserranopaul@gmail.com>
2022-04-12 16:40:27 +0200

    deploy

--- joi/schemas.js
+++ joi/schemas.js
@@ -4,3 +4,14 @@ export const post = Joi.object({
   title: Joi.string().required(),
   body: Joi.string().required()
 });
+
+export const signUp = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
+
+export const signIn = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { getUser, signInUser, signUpUser } from '../controllers/auth.js';
+import validateJOI from '../middlewares/validateJOI.js';
 import verifyToken from '../middlewares/verifyToken.js';
+import { signUp, signIn } from '../joi/schemas.js';
 
 const authRouter = Router();
 
-authRouter.post('/signup', signUpUser);
-authRouter.post('/signin', signInUser);
+authRouter.post('/signup', validateJOI(signUp), signUpUser);
+authRouter.post('/signin', validateJOI(signIn), signInUser);
 authRouter.get('/me', verifyToken, getUser);
 
 export default authRouter;
